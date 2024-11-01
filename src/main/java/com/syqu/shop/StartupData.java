4f80e0ebbc94e4e7cb86ecf7398037e9f61d0e3a
ThuTanPhan0795 <tanthu95bkdn@gmail.com>
2024-11-01 22:06:16 +0700

    fix phan trang

--- src/main/java/com/syqu/shop/StartupData.java
+++ src/main/java/com/syqu/shop/StartupData.java
@@ -34,7 +34,7 @@ public class StartupData implements CommandLineRunner {
         userAccount();
         category();
         for (int i = 0 ; i<= 100 ; i++){
-            exampleProducts();   
+            exampleProducts(i);   
         }
     }
 
@@ -65,20 +65,24 @@ public class StartupData implements CommandLineRunner {
     private void category(){
         Category category1 = new Category();
         Category category2 = new Category();
+        Category category3 = new Category();
         category1.setId(1);
-        category1.setCategoryName("Adventure");
+        category1.setCategoryName("Men's");
         category2.setId(2);
-        category2.setCategoryName("Novel");
+        category2.setCategoryName("Women's");
+        category3.setId(3);
+        category3.setCategoryName("Kid's");
 
         categoryRepository.save(category1);
         categoryRepository.save(category2);
+        categoryRepository.save(category3);
     }
 
-    private void exampleProducts(){
+    private void exampleProducts(int price){
         final String NAME = "Powerful Blender";
         final String IMAGE_URL = "https://raw.githubusercontent.com/ThuTanPhan0795/OmelyApp/refs/heads/fashi/src/main/resources/static/img/products/product-2.jpg";
         final String DESCRIPTION = "A powerful blender for smoothies and shakes.";
-        final BigDecimal PRICE = BigDecimal.valueOf(22);
+        final BigDecimal PRICE = BigDecimal.valueOf(price);
 
         Product product1 = new Product();
         Product product2 = new Product();
@@ -94,61 +98,61 @@ public class StartupData implements CommandLineRunner {
         product1.setName(NAME);
         product1.setImageUrl(IMAGE_URL);
         product1.setDescription(DESCRIPTION);
-        product1.setCategory(categoryRepository.findByCategoryName("Adventure"));
+        product1.setCategory(categoryRepository.findByCategoryName("Women's"));
         product1.setPrice(PRICE);
 
         product2.setName(NAME);
         product2.setImageUrl(IMAGE_URL);
         product2.setDescription(DESCRIPTION);
-        product2.setCategory(categoryRepository.findByCategoryName("Adventure"));
+        product2.setCategory(categoryRepository.findByCategoryName("Men's"));
         product2.setPrice(PRICE);
 
         product3.setName(NAME);
         product3.setImageUrl(IMAGE_URL);
         product3.setDescription(DESCRIPTION);
-        product3.setCategory(categoryRepository.findByCategoryName("Novel"));
+        product3.setCategory(categoryRepository.findByCategoryName("Kid's"));
         product3.setPrice(PRICE);
 
         product4.setName(NAME);
         product4.setImageUrl(IMAGE_URL);
         product4.setDescription(DESCRIPTION);
-        product4.setCategory(categoryRepository.findByCategoryName("Novel"));
+        product4.setCategory(categoryRepository.findByCategoryName("Women's"));
         product4.setPrice(PRICE);
 
         product5.setName(NAME);
         product5.setImageUrl(IMAGE_URL);
         product5.setDescription(DESCRIPTION);
-        product5.setCategory(categoryRepository.findByCategoryName("Novel"));
+        product5.setCategory(categoryRepository.findByCategoryName("Men's"));
         product5.setPrice(PRICE);
 
         product6.setName(NAME);
         product6.setImageUrl(IMAGE_URL);
         product6.setDescription(DESCRIPTION);
-        product6.setCategory(categoryRepository.findByCategoryName("Novel"));
+        product6.setCategory(categoryRepository.findByCategoryName("Kid's"));
         product6.setPrice(PRICE);
 
         product7.setName(NAME);
         product7.setImageUrl(IMAGE_URL);
         product7.setDescription(DESCRIPTION);
-        product7.setCategory(categoryRepository.findByCategoryName("Novel"));
+        product7.setCategory(categoryRepository.findByCategoryName("Women's"));
         product7.setPrice(PRICE);
 
         product8.setName(NAME);
         product8.setImageUrl(IMAGE_URL);
         product8.setDescription(DESCRIPTION);
-        product8.setCategory(categoryRepository.findByCategoryName("Novel"));
+        product8.setCategory(categoryRepository.findByCategoryName("Men's"));
         product8.setPrice(PRICE);
 
         product9.setName(NAME);
         product9.setImageUrl(IMAGE_URL);
         product9.setDescription(DESCRIPTION);
-        product9.setCategory(categoryRepository.findByCategoryName("Novel"));
+        product9.setCategory(categoryRepository.findByCategoryName("Kid's"));
         product9.setPrice(PRICE);
 
         product10.setName(NAME);
         product10.setImageUrl(IMAGE_URL);
         product10.setDescription(DESCRIPTION);
-        product10.setCategory(categoryRepository.findByCategoryName("Novel"));
+        product10.setCategory(categoryRepository.findByCategoryName("Women's"));
         product10.setPrice(PRICE);
 
         productService.save(product1);
